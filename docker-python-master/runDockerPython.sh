0486f88d29d4f31741979b12f33aa325c40cf900
Dan <dan9845@hotmail.com>
2015-06-08 19:13:08 -0400

    Edited index file

--- docker-python-master/runDockerPython.sh
+++
@@ -1,4 +0,0 @@
-
-docker run -d -e VMHOST=192.168.59.103 --name python -p 8000:8000 --link tomcat-maven:tomcat jeffdavisco/polymer-python:latest /local/git/docker-python/run.sh
-
-# once running you can identify the IP addr by using: boot2docker ip. Then, use that IP to access port 8080
